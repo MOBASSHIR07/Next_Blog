b9e0d436148cd34d7a67a65e04cf7de056fd53b1
Sadikul Mobasshir <114331112+MOBASSHIR07@users.noreply.github.com>
2026-01-24 00:39:28 +0600

    Input validation with zod without schema

--- app/(CommonLayout)/register/page.tsx
+++ app/(CommonLayout)/register/page.tsx
@@ -3,10 +3,11 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useForm } from "@tanstack/react-form"
+import * as z from "zod"
 
 export default function RegisterPage() {
 
- // 1 firsrt write this
+  // 1 firsrt write this
   const form = useForm({
     defaultValues: {
       fullName: "",
@@ -29,58 +30,66 @@ export default function RegisterPage() {
 
 
 
-{/* 3 akekta input field */}
-{/* Field 1: Full Name */}
-  <form.Field
-  name="fullName"
-  children={(field) => (
-    <div className="space-y-1">
-      <Label className="text-sm font-semibold ml-1">Full Name</Label>
-      <Input
-        value={field.state.value}
+      {/* 3 akekta input field */}
+      {/* Field 1: Full Name  (form.field holo tanstack ar) */}
+      <form.Field
+        name="fullName"
 
-        onChange={(e) => field.handleChange(e.target.value)}
-        placeholder="John Doe"
-        className="flex h-11 w-full rounded-xl border border-slate-200 bg-slate-50/30 dark:bg-slate-950 px-4 text-sm focus:ring-2 focus:ring-primary focus:outline-none"
+        validators={{ onSubmit: z.string().min(1, "User name Required").min(4, "Minimum 4 character needed") }}
+
+        children={(field) => (
+          <div className="space-y-1">
+            <Label className="text-sm font-semibold ml-1">Full Name</Label>
+            <Input
+              value={field.state.value}
+
+              onChange={(e) => field.handleChange(e.target.value)}
+              placeholder="John Doe"
+              className="flex h-11 w-full rounded-xl border border-slate-200 bg-slate-50/30 dark:bg-slate-950 px-4 text-sm focus:ring-2 focus:ring-primary focus:outline-none"
+            />
+            {field.state.meta.errors.length > 0 && (
+              <p className="text-xs text-red-500 ml-1">
+                {field.state.meta.errors[0]?.message}
+              </p>
+            )}
+          </div>
+        )}
       />
-    </div>
-  )}
-/>
 
-{/* 3.1 akekta input field */}
-  <form.Field
-  name="email"
-  children={(field) => (
-    <div className="space-y-1">
-      <Label className="text-sm font-semibold ml-1">Email</Label>
-      <Input
-        value={field.state.value}
+      {/* 3.1 akekta input field */}
+      <form.Field
+        name="email"
+        children={(field) => (
+          <div className="space-y-1">
+            <Label className="text-sm font-semibold ml-1">Email</Label>
+            <Input
+              value={field.state.value}
 
-        onChange={(e) => field.handleChange(e.target.value)}
-        placeholder="John@gmail.com"
-        className="flex h-11 w-full rounded-xl border border-slate-200 bg-slate-50/30 dark:bg-slate-950 px-4 text-sm focus:ring-2 focus:ring-primary focus:outline-none"
+              onChange={(e) => field.handleChange(e.target.value)}
+              placeholder="John@gmail.com"
+              className="flex h-11 w-full rounded-xl border border-slate-200 bg-slate-50/30 dark:bg-slate-950 px-4 text-sm focus:ring-2 focus:ring-primary focus:outline-none"
+            />
+          </div>
+        )}
       />
-    </div>
-  )}
-/>
-{/* 3.2 akekta input field */}
-  <form.Field
-  
-  name="password"
-  children={(field) => (
-    <div className="space-y-1">
-      <Label className="text-sm font-semibold ml-1">Password</Label>
-      <Input
-        value={field.state.value}
-        type="password"
+      {/* 3.2 akekta input field */}
+      <form.Field
 
-        onChange={(e) => field.handleChange(e.target.value)}
-        placeholder="password"
-        className="flex h-11 w-full rounded-xl border border-slate-200 bg-slate-50/30 dark:bg-slate-950 px-4 text-sm focus:ring-2 focus:ring-primary focus:outline-none"
+        name="password"
+        children={(field) => (
+          <div className="space-y-1">
+            <Label className="text-sm font-semibold ml-1">Password</Label>
+            <Input
+              value={field.state.value}
+              type="password"
+
+              onChange={(e) => field.handleChange(e.target.value)}
+              placeholder="password"
+              className="flex h-11 w-full rounded-xl border border-slate-200 bg-slate-50/30 dark:bg-slate-950 px-4 text-sm focus:ring-2 focus:ring-primary focus:outline-none"
+            />
+          </div>
+        )}
       />
-    </div>
-  )}
-/>
 
 
       <Button type="submit" className="w-full mt-4"> Register</Button>
